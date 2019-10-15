4612b1c797afc014b199bd756583cb943e6e9052
Steven Agri <stevenagri1@gmail.com>
2019-10-15 19:21:11 -0400

    new svg

--- src/component/card.js
+++ src/component/card.js
@@ -8,7 +8,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Joy from '../images/joyoftravel.png';
-import Cawds from './cawds.css';
 import { flexbox } from '@material-ui/system';
 
 const useStyles = makeStyles({
