843afb317570971d897753a7c1e8cbbb95abeed3
Steven Agri <stevenagri1@gmail.com>
2020-01-24 21:13:15 -0500

    adding mail form

--- src/component/header.js
+++ src/component/header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './FooterBox.css';
+import styled from 'styled-components'
+
+const Size = styled.p`
+  font-size: 1em;
+  `;
 
 function Header() {
   return (
     <div className="Head">
-      
+      <Size>
       <h1>Steven's Web Development Portfolio</h1>
-<p>Examples of some of my projects.</p>
+<p>Examples of some of my projects.</p></Size>
     </div>
   );
 }
